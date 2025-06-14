89c19c1df26e562a0945032795a604293c92b91f
Shubham Soni <shubxam@gmail.com>
2025-06-14 23:16:37 +0530

    update comments/notes

--- src/linkhut_lib/__init__.py
+++ src/linkhut_lib/__init__.py
@@ -1,3 +1,6 @@
 """LinkHut Library for interacting with bookmark services."""
 
 __version__ = "0.1.0"
+
+# when importing the linkhub_lib module, the following functions will be automatically available
+# from .linkhut_lib import create_bookmark
