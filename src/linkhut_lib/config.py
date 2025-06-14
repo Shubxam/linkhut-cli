af94a3ca75c5608633bda8e612c91461d6096c95
Shubham Soni <shubxam@gmail.com>
2025-06-14 23:16:46 +0530

    refactor: reorganize LinkHut API endpoints for clarity and consistency

--- src/linkhut_lib/config.py
+++ src/linkhut_lib/config.py
@@ -4,6 +4,10 @@ Configuration settings for the LinkHut and LinkPreview APIs.
 This module contains the base URLs and header templates for making API requests.
 The actual API keys are inserted into these templates at runtime.
 """
+# todo: make use of ENUMs and TypeAlias and Dataclasses
+
+# from enum import Enum
+# from typing import TypeAlias
 
 # LinkHut API configuration
 LINKHUT_HEADER: dict[str, str] = {
@@ -14,10 +18,13 @@ LINKHUT_BASEURL: str = "https://api.ln.ht"
 
 # LinkHut API endpoints
 LINKHUT_API_ENDPOINTS: dict[str, str] = {
-    "bookmark_create": "/v1/posts/add",
-    "tag_suggest": "/v1/posts/suggest",
-    "bookmark_recent": "/v1/posts/recent",
     "bookmark_get": "/v1/posts/get",
+    "bookmark_recent": "/v1/posts/recent",
+    "bookmark_create": "/v1/posts/add",
+    "bookmark_delete": "/v1/posts/delete",
+    "tag_suggest": "/v1/posts/suggest",
+    "tag_delete": "/v1/tags/delete",
+    "tag_rename": "/v1/tags/rename"
 }
 
 
