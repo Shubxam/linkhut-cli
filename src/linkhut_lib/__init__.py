6c8ee1fcb879babe6832f36383b2019a23ecfceb
Shubham Soni <shubxam@gmail.com>
2025-06-16 20:50:01 +0530

    update the project scaffold using uvinit

--- src/linkhut_lib/__init__.py
+++ src/linkhut_lib/__init__.py
@@ -1,6 +1,5 @@
-"""LinkHut Library for interacting with bookmark services."""
-
-__version__ = "0.1.0"
-
-# when importing the linkhub_lib module, the following functions will be automatically available
-# from .linkhut_lib import create_bookmark
+__all__ = (  # noqa: F405
+    # TODO: Add all public symbols here.
+)
+# when importing the linkhut_lib module, the following functions will be automatically available
+from .linkhut_lib import *  # noqa: F403
