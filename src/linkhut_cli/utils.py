7a046841f38a5beebd4b809d8f54530ddf9a82c0
Shubham Soni <shubxam@gmail.com>
2025-06-17 16:05:34 +0530

    fix: remove unused parameter from parse_bulk_items and update TODO for bookmark parsing

--- src/linkhut_cli/utils.py
+++ src/linkhut_cli/utils.py
@@ -6,18 +6,13 @@ def parse_bulk_items(content: str) -> list[str]:
     Parse a string of items(URLs, tags) separated by newlines, commas, whitespace into a list of items.
     Args:
         content (str): A string containing items separated by newlines, commas, whitespace.
-        type (str): The type of items to parse. Can be "url" or "tag".
     Returns:
         list[str]: A list of items.
     """
     return [item.strip() for item in re.split(r"[,\n]+", content) if item.strip()]
 
 
-def parse_linkhut_bookmarks():
-    """
-    Parse a list of dictionaries containing linkhut bookmarks.
-    """
-    pass
+# TODO: Implement the function to parse linkhut get response to display the bookmark entries
 
 
 def sanitize_tags(tag_string: str) -> str:
