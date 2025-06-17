afba82be9510c2fcb623d0f46ebeb28c6acd8eed
Shubham Soni <shubxam@gmail.com>
2025-06-17 16:05:54 +0530

    fix: comment out import statement in __init__.py for clarity

--- src/linkhut_lib/__init__.py
+++ src/linkhut_lib/__init__.py
@@ -2,4 +2,4 @@ __all__ = (  # noqa: F405
     # TODO: Add all public symbols here.
 )
 # when importing the linkhut_lib module, the following functions will be automatically available
-from .linkhut_lib import *  # noqa: F403
+# from .linkhut_lib import *  # noqa: F403
